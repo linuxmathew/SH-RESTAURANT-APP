27ca897089eda949cfebd12651d093c44b8ba5a3
Temitayo Afolabi <linuxmathew1245@gmail.com>
2022-04-27 12:59:56 +0100

    worked on the pizza img display property and removed the navbar height

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,16 +8,16 @@ const Hero = () => {
             <section className="heroSection">
                 <div className="container">
                     <div className="row py-5">
-                        <div className="col-lg-6 col-sm-12 py-5">
-                            <div className="px-5">
+                        <div className="col-12 col-sm-12 col-md-6 col-lg-6 py-5 px-5">
+                            
                                 <h2 className="promo">PROMO!!!</h2>
                                 <h2 className="buy">BUY <br/> TWO <span className="pizza">PIZZAS</span></h2> 
                                 <p className="get">Get one for free</p>
                                 <p className="h5 text-white mb-4">Free Delivery within Lagos</p>
                                 <a className="orderBtn" href="/">ORDER NOW</a>
-                            </div>
+                            
                         </div>
-                        <div className="col-lg-6 .d-sm-none .d-md-none">
+                        <div className="col-12 col-sm-12 col-md-6 col-lg-6">
                             <img className="pizzaImg" src={ PizzaImg } alt="Pizza" />
                         </div>
                     </div>
