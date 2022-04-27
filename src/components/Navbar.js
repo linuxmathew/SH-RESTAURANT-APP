3cf0abbbb97489a655cd33d5437616ef65432123
Temitayo Afolabi <linuxmathew1245@gmail.com>
2022-04-27 12:12:51 +0100

    Navigation links not showing. Changed the link className from nav-a to nav-links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,28 +5,28 @@ import {Link} from 'react-router-dom';
 
 const Navbar = () => {
     return(
-        <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-lg">
-            <div className="container-fluid">
+        <nav className="navbar navbar-light bg-light fixed-top navbar-expand-md">
+            <div className="container">
                 <a className="navbar-brand px-5" href="/">SH FOOD</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
                     aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-a"  to="/">Home</Link>
+                            <Link className="nav-link"  to="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-a" to="/about">About</Link>
+                            <Link className="nav-link" to="/about">About</Link>
                         </li>
 
                         <li className="nav-item">
-                            <Link className="nav-a" to="/product">Product</Link>
+                            <Link className="nav-link" to="/product">Product</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-a" to="/contact">Contact</Link>
+                            <Link className="nav-link" to="/contact">Contact</Link>
                         </li>
                     </ul>
                     <div className="d-flex">
